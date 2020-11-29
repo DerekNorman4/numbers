4ab0e846a197e6406c57aad2578e495e34842e75
Derek Norman <dereknorman42@gmail.com>
2020-11-29 11:31:12 -0800

    numbers.sh completed

---
+++ numbers.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#numbers.sh
+#Derek Norman
+
+echo "Please enter a positive number: "
+read INPUT
+Num=1
+while [ $Num -le $INPUT ]
+do
+	if [ $((Num%2)) -eq 0 ]
+	then
+		echo "$Num even"
+	else
+		echo "$Num odd"
+	fi
+	Num=$((Num+1))
+done
